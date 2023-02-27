e2b8f02368747855cd6acea360fe2562525277c7
Yuliia Stepkina <metulia@mail.ru>
2023-02-27 16:19:25 +0300

    added ClickListener for start_button

--- app/src/main/java/com/example/mykotweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/mykotweatherapp/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.mykotweatherapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonStart: View = findViewById(R.id.start_button)
+        buttonStart.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Начинаем работу приложения",
+                Toast.LENGTH_LONG
+            ).show();
+        }
     }
-
-
 }
