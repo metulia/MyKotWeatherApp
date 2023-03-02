543ef232a3aad3bcb0bad2b02c0818a2ff70e0d1
Yuliia Stepkina <metulia@mail.ru>
2023-03-02 17:26:10 +0300

    added binding

--- app/src/main/java/com/example/mykotweatherapp/view/MainFragment.kt
+++ app/src/main/java/com/example/mykotweatherapp/view/MainFragment.kt
@@ -5,15 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.mykotweatherapp.R
+import com.example.mykotweatherapp.databinding.FragmentMainBinding
+import com.example.mykotweatherapp.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
+
+    lateinit var binding: FragmentMainBinding  // утечка памяти
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        //return inflater.inflate(R.layout.fragment_main, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        val observer = object : Observer<Any> {
+            override fun onChanged(data: Any) {
+                renderData(data)
+            }
+        }
+
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+
+        viewModel.getWeather()
+    }
+
+    private fun renderData(data: Any) {
+        Toast.makeText(requireContext(), "Работает", Toast.LENGTH_SHORT).show()
     }
 
     companion object {
--- app/src/main/java/com/example/mykotweatherapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/mykotweatherapp/viewmodel/MainViewModel.kt
@@ -3,9 +3,17 @@ package com.example.mykotweatherapp.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
 
 class MainViewModel(private val liveData: MutableLiveData<Any> = MutableLiveData()) : ViewModel() {
     fun getData(): LiveData<Any> {
         return liveData
+    }
+
+    fun getWeather() {
+        Thread {
+            sleep(1000L)
+            liveData.postValue(Any())
+        }.start()
     }
 }
