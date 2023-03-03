9eab118fabca380a421cebdadeb2155dba57c06f
Yuliia Stepkina <metulia@mail.ru>
2023-03-03 17:03:34 +0300

    done

--- app/src/main/java/com/example/mykotweatherapp/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/mykotweatherapp/repository/RepositoryImpl.kt
@@ -1,12 +1,13 @@
 package com.example.mykotweatherapp.repository
 
-class RepositoryImpl: Repository {
+class RepositoryImpl : Repository {
     override fun getWeatherFromServer(): Weather {
-        Thread.sleep(2000L)
-        return Weather()
+        Thread.sleep(2000L)         // эмуляция сетевого запроса
+        return Weather()                  // эмуляция ответа
     }
 
     override fun getWeatherFromLocalStorage(): Weather {
-        return Weather()
+        Thread.sleep(200L)      // эмуляция запроса локального
+        return Weather()             // эмуляция ответа
     }
 }
